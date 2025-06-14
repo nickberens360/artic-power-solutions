ab555893596c5742b7dc9d36948fa9e0c6564657
Nick Berens <nickberens360@gmail.com>
2025-06-14 07:05:57 -0500

    WIP tailwind updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ export default {
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                accent: '#fc5252',
+                'accent-dark': '#e52a2a',
+                'custom-black': 'rgb(15, 18, 25)',
+                'custom-gray': 'rgb(96, 115, 159)',
+                'gray-light': 'rgb(229, 233, 240)',
+                'gray-dark': 'rgb(34, 41, 57)',
+                'body-bg': '#050a1c',
+            },
+            fontFamily: {
+                sans: ['Atkinson', 'sans-serif'],
+            },
+            boxShadow: {
+                custom: '0 2px 6px rgba(96, 115, 159, 25%), 0 8px 24px rgba(96, 115, 159, 33%), 0 16px 32px rgba(96, 115, 159, 33%)',
+            },
+        },
     },
     plugins: [],
 } satisfies Config;
